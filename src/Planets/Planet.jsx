23018e722f6476b1dbf68cda9208b532af19c6c9
Om Nigam <om2021064@akgec.ac.in>
2024-02-23 15:55:37 +0530

    adding carousal logic

--- src/Planets/Planet.jsx
+++ src/Planets/Planet.jsx
@@ -4,6 +4,7 @@ import "./planet.css";
 const generatePlanetStyle = (planet) => {
   // Use properties of the planet to influence the randomness
   const seed = (planet.diameter / 100) % 100; // Example seed, use more properties for variation
+  const hasRings = Math.random() < 0.3;
 
   // Generate some random values based on the seed
   const size = Math.floor(seed / 5 + 20); // Adjust size slightly based on the seed
@@ -22,7 +23,7 @@ const generatePlanetStyle = (planet) => {
     width: `${size}rem`,
     height: `${size}rem`,
     background: `radial-gradient(circle at 20% 20%, ${color1}, transparent 80%), 
-                 radial-gradient(circle at 55% 45%, ${color2}, transparent 40%), 
+                 radial-gradient(circle at 55% 45%, ${color2}, transparent 60%), 
                  radial-gradient(circle at 75% 10%, ${color3}, transparent 50%)`,
     boxShadow,
   };
